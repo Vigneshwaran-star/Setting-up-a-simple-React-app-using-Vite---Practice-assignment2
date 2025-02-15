f6d7a9f95ee39874815180e7da8c4957983b8f91
Vigneshwaran-star <nanbanenemy143@gmail.com>
2025-02-15 12:25:00 +0530

    setup simple react

---
+++ my-app/src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Name:Vigneshwaran</h1>
+      <h2>Email:vigneshwaran.p.s76@kalvium.community</h2>
+    </div>
+  )
+}
+
+export default Home
